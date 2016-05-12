b5772ceea0951b6459f79e5bdba70c3db2cbde05
yangkuang <yangkuang@bupt.edu.cn>
2016-05-12 16:58:04 +0800

    Add a boolean variable: hasRun to check whether this service has been executed

--- htmlParseService/src/edu/bupt/service/html/WebpageAdaptation.java
+++ htmlParseService/src/edu/bupt/service/html/WebpageAdaptation.java
@@ -16,6 +16,7 @@ import org.jsoup.select.Elements;
  */
 public class WebpageAdaptation implements ICCNService{
     private CCNIOManage manage;
+    private static boolean hasRun=false;
 
     public WebpageAdaptation(CCNIOManage manage){
         this.manage = manage;
@@ -36,50 +37,55 @@ public class WebpageAdaptation implements ICCNService{
         //String url = "file:///home/yangkuang/source/test.html";
         //File htmlFile = new File("/home/yangkuang/source/test.html");
         //InputStream ips = new FileInputStream(htmlFile);
-        if (args.length > 1) {
-            String htmlFile = "ccnx:/contents/"+args[0];
-            InputStream ips = manage.getCCNFile(htmlFile);
-            ParseHtml htmlParser = new ParseHtml(ips, "UTF-8", "ccnx:/");
-            Document htmlDocument = htmlParser.getDocument();
-            Elements jpgs = htmlDocument.select("img[src$=.jpg]");
+        if (!hasRun) {
+            if (args.length > 1) {
+                String htmlFile = "ccnx:/contents/"+args[0];
+                InputStream ips = manage.getCCNFile(htmlFile);
+                ParseHtml htmlParser = new ParseHtml(ips, "UTF-8", "ccnx:/");
+                Document htmlDocument = htmlParser.getDocument();
+                Elements jpgs = htmlDocument.select("img[src$=.jpg]");
 
-            System.out.println(jpgs.size());
+                System.out.println(jpgs.size());
 
-            for (Element src : jpgs) {
-                if (src.tagName().equals("img")) {
-                    print(" * %s: %sx%s (%s)",
-                            src.tagName(), src.attr("width"), src.attr("height"),
-                            trim(src.attr("alt"), 40));
-                } else {
-                    print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+                for (Element src : jpgs) {
+                    if (src.tagName().equals("img")) {
+                        print(" * %s: %sx%s (%s)",
+                                src.tagName(), src.attr("width"), src.attr("height"),
+                                trim(src.attr("alt"), 40));
+                    } else {
+                        print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+                    }
                 }
-            }
 
-            for (Element src : jpgs) {
-                src.attr("width", "50");
-                src.attr("height", "50");
-            }
-
-            for (Element src : jpgs) {
-                if (src.tagName().equals("img")) {
-                    print(" * %s: %sx%s (%s)",
-                            src.tagName(), src.attr("width"), src.attr("height"),
-                            trim(src.attr("alt"), 40));
-                } else {
-                    print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+                for (Element src : jpgs) {
+                    src.attr("width", "50");
+                    src.attr("height", "50");
                 }
+
+                for (Element src : jpgs) {
+                    if (src.tagName().equals("img")) {
+                        print(" * %s: %sx%s (%s)",
+                                src.tagName(), src.attr("width"), src.attr("height"),
+                                trim(src.attr("alt"), 40));
+                    } else {
+                        print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+                    }
+                }
+
+                CCNFileOutputStream cfo = manage.writeCCNBack(interest);
+                DataOutputStream dot = new DataOutputStream(cfo);
+                DataOutputStream drt = new DataOutputStream(manage.putRepoFile(interest.getContentName().toURIString()));
+                dot.writeChars(htmlDocument.html());
+                drt.writeChars(htmlDocument.html());
+                dot.flush();
+                drt.flush();
+                dot.close();
+                drt.close();
+
+            }else {
+                throw new IOException("args should not be null!!");
             }
-            CCNFileOutputStream cfo = manage.writeCCNBack(interest);
-            DataOutputStream dot = new DataOutputStream(cfo);
-            DataOutputStream drt = new DataOutputStream(manage.putRepoFile(interest.getContentName().toURIString()));
-            dot.writeChars(htmlDocument.html());
-            drt.writeChars(htmlDocument.html());
-            dot.flush();
-            drt.flush();
-            dot.close();
-            drt.close();
-        }else {
-            throw new IOException("args should not be null!!");
+            hasRun = true;
         }
     }
 
